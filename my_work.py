a10dd52c9dfb0267469db77b050c52d6b2995e26
Zhenya <emakolov@bk.ru>
2026-07-25 03:32:26 +0300

    first commit

--- my_work.py
+++ my_work.py
@@ -9,5 +9,15 @@ with open('token.txt') as file_object:
 class VkUser:
     url = 'https://api.vk.ru/method/'
 
-    def __init__(self):
-        pass
+    def __init__(self, token, version):
+        self.params = {'access_token': token, 'v': version}
+
+    def photos_get(self, owner_id):
+        url = self.url + 'photos.get/'
+        params = {**self.params, 'owner_id': owner_id, 'album_id': 'profile', 'rev': 0, 'extended': 1, 'count': 10}
+        req = requests.get(url, params).json()
+        return req
+
+
+obj = VkUser(token, '5.199')
+pprint(obj.photos_get('1'))
